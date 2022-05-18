e296c0ccc42363c1ec6e172bc96be521cd756c63
Long Nguyen <longnp.email@gmail.com>
2022-05-18 22:53:02 +0700

    init project

---
+++ src/main/java/com/project/mememanagement/MemeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.mememanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeManagementApplication.class, args);
+    }
+
+}
